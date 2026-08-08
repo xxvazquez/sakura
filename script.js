2dcfc793fad0e7dea9ad630058ba45a19b38a724
Laura <44951156+xxvazquez@users.noreply.github.com>
2026-08-09 01:14:55 +0200

    default sort by the 3rd column asc

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
         info:false,
         ordering:true,
         searching:true,
+      
+        // Default sort by the 3rd column asc
+        order: [[3, "asc"]],
 
         language:{
             search:"",
@@ -15,3 +18,5 @@ $(document).ready(function () {
     });
 
 });
+
+
